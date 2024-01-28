0d02349354be90e515362c60ff9b13b7c413fa49
VillSource <anirut1800@gmail.com>
2024-01-28 09:57:23 +0700

    change database name

--- src/dao/article.model.ts
+++ src/dao/article.model.ts
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 import { ArticleSchema } from "./schemas/article.shema";
 
-export const ArticleModel = mongoose.model("articles", ArticleSchema);
+export const ArticleModel = mongoose.model("none-detail-articles", ArticleSchema);
