5ebf71a90db5e0eb54bf645324aa2bbf0f6d399e
VillSource <anirut1800@gmail.com>
2024-03-02 19:28:20 +0700

    fix: speling

--- src/workers/add-oficail-account.ts
+++ src/workers/add-oficail-account.ts
@@ -2,7 +2,7 @@ import { updateProfileById } from "../repositories/profile.repository";
 
 export function addSystemProfile() {
   updateProfileById("AAAAAAAAAECAAAAAAAAAAA", {
-    displayName: "เบต้าบล็ก",
+    displayName: "เบต้าบล็อก",
     shortBio:
       "บัญชีทางการของเบต้าบล็อก\nสามารถสร้างชุมชน oppen source ด้วยกันได้ที่ github.com/UntitleCMS",
   });
